4ced92c0562fd13428ef97c62e7fe8972accccc8
Jose Gaston <jmcbgaston@gmail.com>
2020-11-29 14:50:30 -0800

    Add your-profile-reviews

--- frontend/components/your_reviews.jsx
+++ frontend/components/your_reviews.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class YourReviews extends React.Component {
     constructor(props) {
         super(props)
     }
+
+    componentDidMount() {
+        debugger
+
+        this.props.fetchReviews();
+    }
     
     render() {
-        return(
-            <h1>I AM THE REVIEWS COMPONENT</h1>
-        )
+        let reviews = this.props.reviews.map((review, idx) => {
+            return(
+                <li key={idx}>
+                    {review.rating}
+                    {review.body}
+                    <Link to={`/recipes/${review.recipe_id}`}>Go to recipe</Link>
+                </li>
+            )
+        })
+
+        debugger
+        
+        if (reviews.length > 0) {
+            debugger
+            return(
+                <div>
+                    <ul>
+                        {reviews}
+                    </ul>
+                </div>
+            )
+        } else {
+            debugger
+            return(
+                <div>
+                    <ul>
+                        <li>You have no reviews at the moment</li>
+                    </ul>
+                </div>
+            )
+        }
     }
 }
 
--- frontend/components/your_reviews_container.js
+++ frontend/components/your_reviews_container.js
@@ -1,15 +1,33 @@
 import { connect } from 'react-redux'
 import YourReviews from './your_reviews'
+import { fetchReviews } from '../actions/review_actions'
 
 const mSTP = state => {
-    return {
+    debugger
 
+    let allReviews = Object.values(state.entities.reviews)
+    let currUser = state.session.currentUser
+    let myReviews = []
+
+    allReviews.forEach((review) => {
+        if (review.reviewer_id === currUser) {
+            debugger
+
+            myReviews.push(review)
+        }
+    })
+
+    debugger
+
+    return {
+        reviews: myReviews,
+        currUser
     }
 }
 
 const mDTP = dispatch => {
     return {
-
+        fetchReviews: () => dispatch(fetchReviews())
     }
 }
 
